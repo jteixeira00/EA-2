d59cbe1e01cfe5d2facd56d08557f0b1d59844c7
João Teixeira <44053859+jteixeira00@users.noreply.github.com>
2021-04-22 14:51:10 +0100

    100

--- Trabalho2.cpp
+++ Trabalho2.cpp
@@ -11,42 +11,106 @@ int n;       //numero de blocos;
 int h;       //tamanho do bloco;
 int ceiling; //altura do teto -> H no enunciado;
 int narcs;
-vector<vector<int>> table;
+vector<vector<long long>> table;
+long long counter = 0;
+
+long long mod_abs(long long a, int mod)
+{
+    return ((a % mod) + mod) % mod;
+}
+long long mod_add(long long a, int b, int mod)
+{
+    return (mod_abs(a, mod) + mod_abs(b, mod)) % mod;
+}
 
 int arc()
 {
-    int aux;
-    for (int i = 0; i < h; i++)
+    counter = 0;
+    long long aux;
+    int check;
+    int lastcol = 1;
+    long long varaux;
+    /*
+    for (int i = 0; i < ceiling + 1; i++)
     {
         for (int j = 0; j < n + 1; j++)
         {
             table[i][j] = 0;
         }
     }
+    */
     table[h][1] = 1;
-    for (int row = h; row < n + 1; row++)
+    for (int row = h; row < ceiling + 1; row++)
     {
-        for (int col = 1; col < n + 1; col++)
+        check = 0;
+        for (int col = lastcol; col < n + 1; col++)
         {
 
             aux = 0;
             for (int k = row - h + 1; k < row; k++)
             {
-                aux += table[k][col - 1];
+                //aux += table[k][col - 1];
+                aux = mod_add(aux, table[k][col - 1], 1000000007);
             }
             if (row != h || col != 1)
             {
                 table[row][col] = aux;
             }
+
+            if (aux)
+            {
+                if (check == 0)
+                {
+                    lastcol = col;
+                }
+                check = 1;
+            }
+            if (check)
+            {
+                if (aux == 0)
+                {
+                    break;
+                }
+            }
+
+            for (int aux = row - h + 1; aux < row; aux++)
+            {
+                for (int auxcol = 1; auxcol <= n - col; auxcol++)
+                {
+                    //counter += table[row][col] * table[aux][auxcol];
+                    varaux = (long long)(table[row][col] * table[aux][auxcol]) % 1000000007;
+                    counter = mod_add(counter, varaux, 1000000007);
+                }
+            }
+            if (varaux == 0)
+            {
+                break;
+            }
         }
     }
-
-    return narcs;
+    /*
+    for (int row = h + 1; row <= ceiling; row++)
+    {
+        for (int col = 2; col < n; col++)
+        {
+            for (int aux = row - h + 1; aux < row; aux++)
+            {
+                for (int auxcol = 1; auxcol <= n - col; auxcol++)
+                {
+                    //counter += table[row][col] * table[aux][auxcol];
+                    long long varaux = (long long)(table[row][col] * table[aux][auxcol]) % 1000000007;
+                    counter = mod_add(counter, varaux, 1000000007);
+                }
+            }
+        }
+    }
+    */
+    return counter;
 }
 void printMatrix()
 {
     cout << "\n";
-    for (int i = 0; i < n + 1; i++)
+    for (int i = 0; i < ceiling + 1; i++)
     {
         for (int j = 0; j < n + 1; j++)
         {
@@ -66,9 +130,9 @@ int main()
     {
         cin >> n >> h;
         cin >> ceiling;
-        table = vector<vector<int>>(n + 1, vector<int>(n + 1, 0));
-        arc();
-        printMatrix();
+        table = vector<vector<long long>>(ceiling + 1, vector<long long>(n + 1, 0));
+        cout << arc() << "\n";
+        //printMatrix();
     }
     return 0;
 }
