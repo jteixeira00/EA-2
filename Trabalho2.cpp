de58965852f9afc7401ebcf44e745039d78d5b49
João Teixeira <44053859+jteixeira00@users.noreply.github.com>
2021-04-22 20:30:04 +0100

    weird

--- Trabalho2.cpp
+++ Trabalho2.cpp
@@ -22,14 +22,17 @@ long long mod_add(long long a, int b, int mod)
 {
     return (mod_abs(a, mod) + mod_abs(b, mod)) % mod;
 }
-
+long long mod_sub(long long a, int b, int mod)
+{
+    return (mod_abs(a, mod) - mod_abs(b, mod)) % mod;
+}
 int arc()
 {
     counter = 0;
     long long aux;
     int check;
-    int lastcol = 1;
-    long long varaux;
+    int lastcol = 2;
+
     /*
     for (int i = 0; i < ceiling + 1; i++)
     {
@@ -47,11 +50,17 @@ int arc()
         {
 
             aux = 0;
-            for (int k = row - h + 1; k < row; k++)
+
+            /*for (int k = row - h + 1; k < row; k++)
             {
                 //aux += table[k][col - 1];
                 aux = mod_add(aux, table[k][col - 1], 1000000007);
             }
+*/
+            //aux = mod_add(mod_sub(table[row - 1][col], table[row - h][col - 1], 1000000007), table[row - 1][col - 1], 1000000007);
+            aux = mod_sub(mod_add(table[row - 1][col], table[row - 1][col - 1], 1000000007), table[row - h][col - 1], 1000000007);
+            //6 = 4-1+3
+
             if (row != h || col != 1)
             {
                 table[row][col] = aux;
@@ -72,7 +81,7 @@ int arc()
                     break;
                 }
             }
-
+            /*
             for (int aux = row - h + 1; aux < row; aux++)
             {
                 for (int auxcol = 1; auxcol <= n - col; auxcol++)
@@ -82,29 +91,45 @@ int arc()
                     counter = mod_add(counter, varaux, 1000000007);
                 }
             }
-            if (varaux == 0)
+            */
+            long long auxcounter = 0;
+
+            if (col < (n) / 2)
             {
-                break;
-            }
-        }
-    }
-    /*
-    for (int row = h + 1; row <= ceiling; row++)
-    {
-        for (int col = 2; col < n; col++)
-        {
-            for (int aux = row - h + 1; aux < row; aux++)
-            {
-                for (int auxcol = 1; auxcol <= n - col; auxcol++)
+                //cout << "primeiro caso na posicao " << row << " " << col << "\n";
+                for (int auxcol = 1; auxcol <= col; auxcol++)
                 {
-                    //counter += table[row][col] * table[aux][auxcol];
-                    long long varaux = (long long)(table[row][col] * table[aux][auxcol]) % 1000000007;
-                    counter = mod_add(counter, varaux, 1000000007);
+                    auxcounter = mod_add(auxcounter, table[row][auxcol], 1000000007);
                 }
+                counter = mod_add(counter, auxcounter * table[row][col] * 2, 1000000007);
+            }
+
+            else if ((col == (int)(n + 1) / 2))
+            {
+                //cout << "segundo caso na posicao " << row << " " << col << "\n";
+                for (int auxcol = 1; auxcol < col; auxcol++)
+                {
+                    auxcounter = mod_add(auxcounter, table[row][auxcol], 1000000007);
+                    //  cout << auxcounter << "\n";
+                }
+                counter = mod_add(counter, auxcounter * table[row][col] * 2, 1000000007);
+                counter = mod_add(counter, table[row][col] * table[row][col], 1000000007);
+                //cout << "counter: " << counter << "\n";
+            }
+            else
+            {
+                //cout << "terceiro caso na posicao " << row << " " << col << "\n";
+                for (int auxcol = 1; auxcol <= n - col + 1; auxcol++)
+                {
+                    auxcounter = mod_add(auxcounter, table[row][auxcol], 1000000007);
+                    //cout << auxcounter << "\n";
+                }
+                counter = mod_add(counter, auxcounter * table[row][col] * 2, 1000000007);
+                //cout << "counter: " << counter << "\n";
             }
         }
     }
-    */
+
     return counter;
 }
 void printMatrix()
@@ -125,9 +150,11 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int ncases;
+
     cin >> ncases;
     for (int i = 0; i < ncases; i++)
     {
+
         cin >> n >> h;
         cin >> ceiling;
         table = vector<vector<long long>>(ceiling + 1, vector<long long>(n + 1, 0));
